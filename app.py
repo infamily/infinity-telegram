08ed997a81847605b337cdff5478d49bdaffbe3f
Eugeniy Kuznetsov <atorich@gmail.com>
2017-10-17 23:45:04 +0700

    update app bootstrapping

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 # coding: utf-8
 from envparse import Env
 from flask import Flask
+from werkzeug.utils import import_string
 
 env = Env()
 settings_module = env('SETTINGS_MODULE', 'config.settings.local')
 
-app = Flask(__name__)
-app.config.from_object(settings_module)
+
+def create_app():
+    appl = Flask(__name__)
+    appl.config.from_object(settings_module)
+    register_extensions(appl)
+    return appl
+
+
+def register_extensions(appl):
+    for path in appl.config.get('EXTENSIONS', []):
+        cls = import_string(path)
+        extension = cls()
+        extension.init_app(appl)
+
+
+app = create_app()
--- config/settings/base.py
+++ config/settings/base.py
@@ -17,3 +17,15 @@ env.read_envfile(os.path.join(BASE_DIR, '.env'))
 DEBUG = False
 SECRET_KEY = None
 TESTING = False
+
+
+EXTENSIONS = [
+    'apps.telegram_bot.base.TelegramBot',
+]
+
+
+TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN', '')
+TELEGRAM_BOT_FACTORY = env('TELEGRAM_BOT_FACTORY', 'inftybot.bot.create_bot')
+TELEGRAM_BOT_FACTORY_PARAMS = env('TELEGRAM_BOT_FACTORY_PARAMS', {
+    'token': TELEGRAM_BOT_TOKEN
+})
