2a6178b2c880f885e9ce81ced6cd8146b609b6cd
Eugeniy Kuznetsov <atorich@gmail.com>
2017-10-17 19:52:53 +0700

    read env file

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,11 +1,18 @@
 # coding: utf-
 import os
 
+from envparse import Env
+
 BASE_DIR = os.path.abspath(
     os.path.join(
         os.path.dirname(__file__), os.pardir, os.pardir
     )
 )
+
+
+env = Env()
+env.read_envfile(os.path.join(BASE_DIR, '.env'))
+
 
 DEBUG = False
 SECRET_KEY = None
