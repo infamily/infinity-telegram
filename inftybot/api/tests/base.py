d7f4fcb0657fc2c8c23ad83ad4360b7094688dca
Eugeniy Kuznetsov <atorich@gmail.com>
2017-11-09 02:21:59 +0700

    refactor tests

---
+++ inftybot/api/tests/base.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from unittest import TestCase
+
+from mock import patch
+
+from inftybot.api import API
+from inftybot.tests.base import MockResponse
+
+
+class APIMixin(TestCase):
+    def create_api_client(self):
+        """Returns dummy api client"""
+        api = API(base_url='http://example.com/api/v1')
+        return api
+
+
+class AuthenticatedAPIMixin(APIMixin):
+    def create_api_client(self):
+        api = super(AuthenticatedAPIMixin, self).create_api_client()
+        api.session.api_token = 'test_token'
+        api.session.user = {}
+        return api
+
+
+def create_api_response(status_code, content=None, headers=None):
+    """Returns MockResponse with parameters from arguments"""
+    return MockResponse(status_code, content, headers)
+
+
+def patch_api_request(status_code, json):
+    """
+    Decorator
+    Patches requests.sessions.Session.send for mocking API requests
+    """
+    return patch(
+        'requests.sessions.Session.send',
+        return_value=create_api_response(status_code, json),
+    )
--- inftybot/tests/base.py
+++ inftybot/tests/base.py
@@ -6,7 +6,6 @@ from unittest import TestCase
 from mock import patch, MagicMock
 from telegram import User
 
-from inftybot.api import API
 from inftybot.factory import create_bot
 
 
@@ -56,31 +55,6 @@ class MockResponse(object):
         if isinstance(self.content, dict):
             return self.content
         return json.loads(self.content)
-
-
-class APIMixin(TestCase):
-    def create_api_client(self):
-        api = API(base_url='http://example.com/api/v1')
-        return api
-
-
-class AuthenticatedAPIMixin(APIMixin):
-    def create_api_client(self):
-        api = super(AuthenticatedAPIMixin, self).create_api_client()
-        api.session.api_token = 'test_token'
-        api.session.user = {}
-        return api
-
-
-def create_api_response(status_code, content=None, headers=None):
-    return MockResponse(status_code, content, headers)
-
-
-def patch_api_request(status_code, json):
-    return patch(
-        'requests.sessions.Session.send',
-        return_value=create_api_response(status_code, json),
-    )
 
 
 def mock_update(update):
