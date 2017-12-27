7bce6a0bfc3bf4b8dd0c0845ba07307da85c5743
Eugeniy Kuznetsov <atorich@gmail.com>
2017-12-28 02:17:33 +0700

    production logging setup

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,3 +2,33 @@
 from .base import *
 
 DEBUG = False
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'inftybot': {
+            'handlers': ['default'],
+            'level': 'WARNING',
+            'propagate': True
+        },
+        'telegram': {
+            'handlers': ['default'],
+            'level': 'WARNING',
+            'propagate': True
+        }
+    }
+}
+
