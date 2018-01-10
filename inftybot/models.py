da731cfcb3a1b579b1bda146322a3bffba446b2b
Eugeniy Kuznetsov <atorich@gmail.com>
2018-01-11 00:07:55 +0700

    refactor models, added Type model

--- inftybot/models.py
+++ inftybot/models.py
@@ -1,18 +1,26 @@
 # coding: utf-8
 from schematics.models import Model as BaseModel
-from schematics.types import StringType as BaseStringType, IntType, URLType, ListType, EmailType
+from schematics.types import StringType as BaseStringType, IntType, URLType, ListType, EmailType, BaseType, serializable
 from schematics.exceptions import ValidationError
+from inftybot import constants
+
+
+def from_native(cls, data):
+    instance = cls()
+    for key, value in data.items():
+        if hasattr(cls, key):
+            setattr(instance, key, value)
+    return instance
 
 
 class Model(BaseModel):
     """Simple data model"""
+    class Meta:
+        plural = None
+
     @classmethod
     def from_native(cls, data):
-        instance = cls()
-        for key, value in data.items():
-            if hasattr(cls, key):
-                setattr(instance, key, value)
-        return instance
+        return from_native(cls, data)
 
 
 class StringType(BaseStringType):
@@ -22,13 +30,32 @@ class StringType(BaseStringType):
         return value
 
 
+class Type(BaseModel):
+    """Type meta model"""
+    class Meta:
+        plural = 'types'
+
+    id = IntType(required=False)
+    url = URLType(required=False)
+    name = StringType(required=True)
+    definition = StringType(required=False)
+
+
 class Topic(Model):
+    class Meta:
+        plural = 'topics'
+
     id = IntType(required=False)
     type = IntType(required=True)
     title = StringType(required=True, min_length=1)
     body = StringType(required=True, min_length=1)
+    categories = ListType(StringType, default=[], required=False)
     url = URLType(required=False)
     parents = ListType(StringType, required=True, default=[])
+
+    @serializable
+    def type_str(self):
+        return constants.TOPIC_TYPE_CHOICES.get(self.type)
 
 
 class User(Model):
