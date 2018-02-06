1c54d6d14fe51e7b2e70e022ad1db580c0f383c6
Eugeniy Kuznetsov <atorich@gmail.com>
2018-02-06 23:06:22 +0700

    specify categories on newtopic

--- inftybot/intents/newtopic.py
+++ inftybot/intents/newtopic.py
@@ -25,7 +25,7 @@ class TopicCreateCommandIntent(AuthenticatedMixin, BaseTopicIntent, BaseCommandI
 
         keyboard = CHOOSE_TYPE_KEYBOARD
         self.update.message.reply_text(
-            _("Please, choose:"), reply_markup=InlineKeyboardMarkup(keyboard)
+            _("Please, choose type:"), reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
         return states.TOPIC_STATE_TYPE
@@ -38,7 +38,19 @@ class InputTypeIntent(BaseTopicIntent, BaseCallbackIntent):
         self.set_topic(topic)
         self.bot.sendMessage(
             chat_id=self.update.callback_query.message.chat_id,
-            text=_("Well! Please, tell me the topic title"),
+            text=_("Please, enter some categories (comma-separated)"),
+        )
+        return states.TOPIC_STATE_CATEGORY
+
+
+class InputCategoryIntent(BaseTopicIntent, BaseMessageIntent):
+    def handle(self, *args, **kwargs):
+        topic = self.get_topic()
+        topic.categories_str = self.update.message.text
+        self.set_topic(topic)
+        self.bot.sendMessage(
+            chat_id=self.update.message.chat_id,
+            text=_("Ok! Please, input the topic title"),
         )
         return states.TOPIC_STATE_TITLE
 
@@ -77,6 +89,7 @@ class TopicConversationIntent(BaseConversationIntent):
             states={
                 states.TOPIC_STATE_TYPE: [InputTypeIntent.get_handler()],
                 states.TOPIC_STATE_TITLE: [InputTitleIntent.get_handler()],
+                states.TOPIC_STATE_CATEGORY: [InputCategoryIntent.get_handler()],
                 states.TOPIC_STATE_BODY: [InputBodyIntent.get_handler()],
             },
             fallbacks=[
