402d199942aa0c71dde7c4fdafeb0a4fe0c2ad51
Eugeniy Kuznetsov <atorich@gmail.com>
2018-03-02 02:32:13 +0700

    logging

--- inftybot/tasks/base.py
+++ inftybot/tasks/base.py
@@ -1,7 +1,12 @@
 # coding: utf-8
+import logging
+
 from inftybot.config import TELEGRAM_BOT_TOKEN
 from inftybot.factory import create_bot
 from inftybot.storage import ChatDataStorage, UserDataStorage
+
+
+logger = logging.getLogger(__name__)
 
 
 def task(func):
@@ -12,6 +17,8 @@ def task(func):
 
     def wrapper(*args, **kwargs):
         kwargs.update({'bot': bot, 'chat_storage': chat_storage, 'user_storage': user_storage})
+        logger.debug('Run task {}, args: {}, kwargs: {}'.format(func, args, kwargs))
         result = func(*args, **kwargs)
+        logger.debug('Task complete, result: {}'.format(result))
         return result
     return wrapper
