23737e9ff296dffb39f4f2e1ec2f323e88f36021
Eugeniy Kuznetsov <atorich@gmail.com>
2018-03-20 19:51:30 +0700

    fix getcategories chat validation

--- src/inftybot/categories/intents/groupcategories.py
+++ src/inftybot/categories/intents/groupcategories.py
@@ -1,15 +1,23 @@
 # coding: utf-8
 """Intents for handling get/set group/channel categories set"""
+import telegram.error
 from django.utils.translation import gettext
 from telegram.ext import CommandHandler
 
 import inftybot.chats.utils
 from inftybot.authentication.intents.base import AuthenticatedMixin
 from inftybot.chats.models import Chat
-from inftybot.core.exceptions import CommunityRequiredError, AdminRequiredError, ValidationError
+from inftybot.core.exceptions import CommunityRequiredError, AdminRequiredError, ValidationError, ChatNotFoundError
 from inftybot.core.intents.base import BaseCommandIntent, ArgparseMixin
 
 _ = gettext
+
+
+def get_chat(bot, id_or_name):
+    try:
+        return inftybot.chats.utils.get_chat(bot, id_or_name)
+    except telegram.error.BadRequest:
+        raise AdminRequiredError()
 
 
 def get_chat_storage(chat_id):
@@ -41,7 +49,10 @@ class SetCategoriesCommandIntent(AuthenticatedMixin, ArgparseMixin, BaseCommandI
             raise ValidationError(message)
 
         self.parsed_args = self.parser.parse_args(kwargs.get('args'))
-        chat = inftybot.chats.utils.get_chat(self.bot, self.parsed_args.chat)
+        chat = get_chat(self.bot, self.parsed_args.chat)
+
+        if not chat:
+            raise ChatNotFoundError()
 
         if not inftybot.chats.utils.get_chat_is_community(self.bot, chat):
             raise CommunityRequiredError()
@@ -50,7 +61,7 @@ class SetCategoriesCommandIntent(AuthenticatedMixin, ArgparseMixin, BaseCommandI
             raise AdminRequiredError()
 
     def handle(self, *args, **kwargs):
-        chat = inftybot.chats.utils.get_chat(self.bot, self.parsed_args.chat)
+        chat = get_chat(self.bot, self.parsed_args.chat)
         categories = self.parsed_args.categories.split(',')
         storage = get_chat_storage(chat.id)
         storage.data['categories'] = categories
@@ -80,7 +91,10 @@ class GetCategoriesCommandIntent(AuthenticatedMixin, ArgparseMixin, BaseCommandI
             raise ValidationError(message)
 
         self.parsed_args = self.parser.parse_args(kwargs.get('args'))
-        chat = inftybot.chats.utils.get_chat(self.bot, self.parsed_args.chat)
+        chat = get_chat(self.bot, self.parsed_args.chat)
+
+        if not chat:
+            raise ChatNotFoundError()
 
         if not inftybot.chats.utils.get_chat_is_community(self.bot, chat):
             raise CommunityRequiredError()
