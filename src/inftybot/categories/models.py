903f3d66d8fdfc609d6c600b0693a2b960129b54
Eugeniy Kuznetsov <atorich@gmail.com>
2018-03-20 23:37:55 +0700

    cosmetic

--- src/inftybot/categories/models.py
+++ src/inftybot/categories/models.py
@@ -2,15 +2,20 @@
 from django.db import models
 from django.utils.translation import gettext
 
+from inftybot.categories.constants import MAX_CATEGORY_NAME_LENGTH
+
 _ = gettext
 
 
 class Type(models.Model):
+    """
+    Model for Infty API "Type" entity that represents a Category (when proper type is set)
+    """
     class Meta:
         managed = False
         verbose_name = 'Type'
-        verbose_name_plural = 'Types'
+        verbose_name_plural = 'Types'  # used to generate API endpoint URL (see `infinity.api.utils.get_model_resource`)
 
     url = models.URLField(verbose_name='URL')
-    name = models.CharField(max_length=100, verbose_name=_('Name'))
-    definition = models.CharField(max_length=100, verbose_name=_('Definition'))
+    name = models.CharField(max_length=MAX_CATEGORY_NAME_LENGTH, verbose_name=_('Name'))
+    definition = models.CharField(max_length=MAX_CATEGORY_NAME_LENGTH, verbose_name=_('Definition'))
