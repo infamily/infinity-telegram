5989cbbcfdef8526cbb52fc17768db8d1c8ed91a
Eugeniy Kuznetsov <atorich@gmail.com>
2018-03-21 02:00:30 +0700

    notification tasks & tests

---
+++ src/inftybot/notifications/tasks.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+from langsplit import splitter
+
+from infinity.api.base import create_api_client
+from inftybot.categories.models import CategoriesSet
+from inftybot.chats.models import Chat
+from inftybot.tasks.base import task
+from inftybot.topics.models import Topic
+from inftybot.topics.serializers import TopicSerializer
+
+api = create_api_client()
+
+
+@task
+def notify_subscribers_about_new_topic(bot, **kwargs):
+    """
+    Event handler for "new topic created" event.
+    Sends notifications to subscribers
+    """
+
+    # Entities: Chat, Topic, Category
+
+    # *. Fetch topic data from API
+    # *. Instantiate it ?
+    # *. Retrievee its categories
+    # *. Get chats that subscribed to theese categories
+    # *. Send message to every chat from last point
+    pk = kwargs.get('event', {}).get('topic_id')
+
+    if not pk:
+        return
+
+    data = api.topics(pk).get()
+    serializer = TopicSerializer(data=data)
+
+    if not serializer.is_valid():
+        return
+
+    categories = []
+    instance = Topic(**serializer.validated_data)
+    splitted = [splitter.split(value) for value in instance.categories_names]
+
+    for value in splitted:
+        categories.extend(value.values())
+
+    if not categories:
+        return
+
+    categories_queryset = CategoriesSet.objects.by_categories(categories).values('chat_id')
+    chats_queryset = Chat.objects.filter(id__in=categories_queryset).all()
+
+    for chat in chats_queryset.iterator():
+        bot.send_message(chat.id, 'Hello')
